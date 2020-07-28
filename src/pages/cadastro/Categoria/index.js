cdf2c15b5ae464ad11e737c65ac34b91f9d966f2
Andre Girao <andrelgirao22@hotmail.com>
2020-07-28 20:48:54 -0300

    iniciado criacao formulario cadastro categoria

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,17 @@ export default function CadastroCategoria() {
     return (
         <PageDefault>
             <h1>Cadastro de Cateoria</h1>
+
+            <form action="">
+
+                <label htmlFor="">
+                    Nome da Categoria
+                    <input type="text"/>
+                </label>
+
+                <button>Cadastrar</button>
+            </form>
+
             <Link to="/">
                 Ir para Home
             </Link>
